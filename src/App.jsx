8f09ffe98071f78c1f7e3599d7e902ed39e8da6c
Maria <zweitesherz@mail.ru>
2020-09-09 16:58:45 +0500

    header start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { hot } from 'react-hot-loader/root';
+import { hot } from 'react-hot-loader/root'
+import React, { Fragment } from 'react'
+import Header from './Components/Header/Header'
 
-import React from 'react';
-
-
-const App = () => (
-    <div>Start</div>
-);
-
-export default hot(App);
+class App extends React.Component {
+  render () {
+    return (
+      <Fragment>
+        <Header/>
+      </Fragment>
+    )
+  }
+}
+export default hot(App)
