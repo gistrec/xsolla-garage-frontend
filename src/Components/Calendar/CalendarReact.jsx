cf71ac290fc3d22237fb1a5a927e4bde03d69fbe
Chtistina Rozenkova <swisschizz@inbox.ru>
2020-09-10 12:53:04 +0500

    Components update

--- src/Components/Calendar/CalendarReact.jsx
+++ src/Components/Calendar/CalendarReact.jsx
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react'
 import Calendar from 'react-calendar'
+import styles from './CalendarStyles.css'
 import 'react-calendar/dist/Calendar.css';
 
 const CalendarReact = () => {
   return (
     <Fragment>
-      <Calendar/>
+      <Calendar className={styles.calendar}/>
     </Fragment>
   )
 }
