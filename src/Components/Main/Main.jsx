6eb7b3b57b321118c9261861a1084e0c5f34ec2f
VladislavMaksimov <workmaximov@yandex.ru>
2020-09-15 12:55:34 +0500

    В WorkspaceHeader поправил стили. Убрал лишние блоки, сделал отступы с помощью padding у хэдера, а не margin'ов у элемента внутри него. Поменял пиксели на адаптивные единицы измерения.

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,12 +1,11 @@
 import React, { Fragment } from 'react'
-import styles from './MainStyles.module.css'
-import CalendarAndHello from '../CalendarAndHello/CalendarAndHello'
+import { main } from './MainStyles.module.css'
 import Workspace from '../Workspace/Workspace'
 
 // eslint-disable-next-line no-unused-vars
 const Main = () => {
   return (
-    <main className={styles.main}>
+    <main className={main}>
       <Workspace/>
     </main>
   )
