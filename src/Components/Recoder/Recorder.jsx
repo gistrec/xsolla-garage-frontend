123ce34c0e64b70b70ed0b3c599803a75092f618
Chtistina Rozenkova <swisschizz@inbox.ru>
2020-09-16 19:26:55 +0500

    Voice recognition - prepared for merge

--- src/Components/Recoder/Recorder.jsx
+++ src/Components/Recoder/Recorder.jsx
@@ -5,76 +5,72 @@ import axios from "axios";
 
 
 const Recorder = () => {
-    const { transcript, resetTranscript } = useSpeechRecognition()
-    const url = "http://35.184.198.167:8081";
-  
-    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-      return null
-    }
+  const { transcript, resetTranscript } = useSpeechRecognition()
+  const url = "http://35.184.198.167:8081";
 
-    const sendText = () => {
-      const api = '/task';
-      const data =  {
-        "user_id": 9001,
-        "title": "Тест",
-        "text_content": transcript,
-        "date_create": "2020-09-08 17:17:52",
-        "date_target": "2020-09-20 17:17:52",
-        "is_important": true,
-        "is_urgent": true
-      };
-      /*const options = {
-        method: 'POST',
-        url: url + api,
-        data: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        }
-      };*/
-
-      fetch(url + api, {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: {
-          'Content-Type': 'application/json'
-              },
-          body: JSON.stringify(data)
-      })
-
-      /*axios(options)
-        .then(res => console.log(res))
-        .then(() => getTasks())
-        .catch(err => console.dir(err))*/
-    }
-    
-    const getTasks = () => {
-      const api = '/task';
-      const options = {
-        method: 'GET',
-        url: url + api,
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
-      };
-
-      axios.get(url + api)
-        .then(res => console.log(res))
-        .catch(err => console.dir(err));
-    }
-  
-    return (
-      <div>
-        <button onClick={SpeechRecognition.startListening}>Start</button>
-        <button onClick={() => {
-          SpeechRecognition.stopListening;
-          getTasks()
-        }}>
-          Stop
-        </button>
-        <button onClick={resetTranscript}>Reset</button>
-        <p>{transcript}</p>
-      </div>
-    )
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return null
   }
-  export default Recorder
+
+  const sendText = () => {
+    const api = '/task';
+    const data = {
+      //   "user_id": 9001,
+      "title": "Тестирование голосового ввода",
+      "text_content": transcript,
+      //   "date_create": "2020-09-08 17:17:52",
+      //   "date_target": "2020-09-20 17:17:52",
+      //   "is_important": true,
+      //   "is_urgent": true
+    };
+    /*const options = {
+      method: 'POST',
+      url: url + api,
+      data: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    };*/
+
+    fetch(url + api, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+
+    /*axios(options)
+      .then(res => console.log(res))
+      .then(() => getTasks())
+      .catch(err => console.dir(err))*/
+  }
+
+  const getTasks = () => {
+    const api = '/task';
+    const options = {
+      method: 'GET',
+      url: url + api,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+
+    axios.get(url + api)
+      .then(res => console.log(res))
+      .catch(err => console.dir(err));
+  }
+
+  return (
+    <div>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={() => { SpeechRecognition.stopListening }}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <button onClick={() => { sendText() }}>Send Text</button>
+      <p>{transcript}</p>
+    </div>
+  )
+}
+export default Recorder
