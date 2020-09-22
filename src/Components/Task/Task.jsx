519dc58d21ef4413db041b696a256a1d5ea5ea72
Chtistina Rozenkova <swisschizz@inbox.ru>
2020-09-22 14:18:30 +0500

    Minor corrections

--- src/Components/Task/Task.jsx
+++ src/Components/Task/Task.jsx
@@ -71,7 +71,7 @@ const Task = ({ id, title, bodyTask }) => {
           <div className={styles.CheckboxTitleWrapper}>
             <input type="checkbox" className={styles.Checkbox}/>
             <div className={styles.TitleDataWrapper}>
-              <input maxLength="100" className={styles.Title} defaultValue={title}/>
+              <input maxLength="100" placeholder="Add task title here" className={styles.Title} defaultValue={title}/>
               <time>2020-09-20, 12:00</time>
             </div>
           </div>
