f222961147bcf112a6ff54e1931e949d8b15b831
Chtistina Rozenkova <swisschizz@inbox.ru>
2020-09-24 17:07:07 +0500

    Single-line tags, max 30 characters

--- src/Components/Workspace/Children/Tags.jsx
+++ src/Components/Workspace/Children/Tags.jsx
@@ -30,7 +30,7 @@ const Tags = props => {
         ))}
         <form className={styles.addTag} onSubmit={event => addTags(event)}>
           <button className={styles.addTagButton} type={'submit'}>+</button>
-          <input type="text" maxLength="50" placeholder="Добавьте тег" onChange={e => setInput(e.target.value)}/>
+          <input type="text" maxLength="30" placeholder="Добавьте тег" onChange={e => setInput(e.target.value)}/>
         </form>
       </ul>
     </div>
