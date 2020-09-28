f57f4bc8b0932d04bb3ecd7ccad8a5e46b8d6281
Chtistina Rozenkova <swisschizz@inbox.ru>
2020-09-28 17:49:48 +0500

    Authorization page

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import 'regenerator-runtime/runtime'
+import SignIn from './Components/SignIn/SignIn'
+//import App from './App'
 
-import App from './App'
+/* render(<App />, document.getElementById('root')) */
 
-render(<App />, document.getElementById('root')
-)
+render(<SignIn />, document.getElementById('root'))
