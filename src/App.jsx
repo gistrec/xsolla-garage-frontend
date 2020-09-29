835f58409c377f94303f799a64bbee30af91adcd
Chtistina Rozenkova <swisschizz@inbox.ru>
2020-09-29 17:10:01 +0500

    Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { hot } from 'react-hot-loader/root'
 import React from 'react'
+import SignIn from './Components/SignIn/SignIn'
 import Header from './Components/Header/Header'
 import SideHeader from './Components/GridComponents/SideHeader'
 import SideMain from './Components/GridComponents/SideMain'
@@ -11,18 +12,19 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      isSignedIn: false,
       tasks: []
     }
   }
 
-  componentDidMount() {
+  /*componentDidMount() {
     fetch(`https://garage-best-team-ever.tk/task`).then(data => data.json()).then(json => {this.setState({ tasks: json }); console.log(json) })
-  }
+  } */
 
   render() {
     const tasksCount = this.state.tasks.length
 
-    return (
+    return ( !this.state.isSignedIn ? <SignIn /> : 
       <div className={styles.page}>
         <SideHeader />
         <Header />
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import 'regenerator-runtime/runtime'
-import SignIn from './Components/SignIn/SignIn'
-//import App from './App'
+import App from './App'
 
-/* render(<App />, document.getElementById('root')) */
-
-render(<SignIn />, document.getElementById('root'))
+render(<App />, document.getElementById('root'))
