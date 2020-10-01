f4ba9c153c7f279483f220cc0be2188fb2d9cce8
Maria <zweitesherz@mail.ru>
2020-10-01 16:33:27 +0500

    hot-fix расположения кнопок создать и отзыв

--- src/Components/Workspace/Children/WorkspaceHeader.jsx
+++ src/Components/Workspace/Children/WorkspaceHeader.jsx
@@ -9,8 +9,11 @@ const WorkspaceHeader = (props) => {
   return (
     <div className={styles.header}>
       <h2 className={styles.title}>Задачи</h2>
-      <button className={styles.addTask} onClick={renderTask}>+ СОЗДАТЬ</button>
-      <a className={styles.feedback} href="https://docs.google.com/forms/d/e/1FAIpQLSdeeQjNqBTcICcR90D17vcC9OFtep4u3UUd3ge9TzMFcBKs0g/viewform?usp=sf_link" target="_blank">Отзыв</a>
+      <div>
+        <button className={styles.addTask} onClick={renderTask}>+ СОЗДАТЬ</button>
+        <a className={styles.feedback} href="https://docs.google.com/forms/d/e/1FAIpQLSdeeQjNqBTcICcR90D17vcC9OFtep4u3UUd3ge9TzMFcBKs0g/viewform?usp=sf_link" target="_blank">Отзыв</a>
+      </div>
+
     </div>
   )
 }
