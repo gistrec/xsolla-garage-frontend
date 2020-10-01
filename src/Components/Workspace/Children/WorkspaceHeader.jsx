515a10941c5153effb760b2f92e7bfff0504c3e3
Chtistina Rozenkova <swisschizz@inbox.ru>
2020-10-01 17:35:56 +0500

    Minor changes to markup

--- src/Components/Workspace/Children/WorkspaceHeader.jsx
+++ src/Components/Workspace/Children/WorkspaceHeader.jsx
@@ -10,7 +10,7 @@ const WorkspaceHeader = (props) => {
     <div className={styles.header}>
       <h2 className={styles.title}>Задачи</h2>
       <div>
-        <button className={styles.addTask} onClick={renderTask}>+ СОЗДАТЬ</button>
+        <button className={styles.addTask} onClick={renderTask}>Создать</button>
         <a className={styles.feedback} href="https://docs.google.com/forms/d/e/1FAIpQLSdeeQjNqBTcICcR90D17vcC9OFtep4u3UUd3ge9TzMFcBKs0g/viewform?usp=sf_link" target="_blank">Отзыв</a>
       </div>
 
